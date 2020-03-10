885396a6af4728097ceb4f16782b606d326f2894
Mohammad Sabik Irbaz <35444388+msi1427@users.noreply.github.com>
2020-03-10 12:10:04 +0600

    Finished Task

---
+++ Online Courses/Machine Learning(NG)/Codes/Advanced Optimization.m
@@ -0,0 +1,12 @@
+function [jVal, gradient] = costFuntion(theta)
+  jVal = (theta(1)-5)^2 + (theta(2)-5)^2;
+  gradient = zeros(2,1);
+  gradient(1) = 2*(theta(1)-5);
+  gradient(2) = 2*(theta(2)-5);
+endfunction
+
+options = optimset('Gradobj','on','MaxIter','100');
+inititalTheta = zeros(2,1);
+[opTheta, functionVal,exitFlag]=fminunc(@costFuntion,inititalTheta,options)
+
+%theta must be 2D or 1D to work out
---
+++ Online Courses/Machine Learning(NG)/Codes/CostFunction.m
@@ -0,0 +1,5 @@
+function J = CostFunction(X,y,theta)
+m=size(X,1);
+predictions = X*theta;
+errors = (predictions-y) .^ 2;
+J = 1/(2*m) * sum(errors);
